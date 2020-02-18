07e1d20f6efbe2b9445632989208625ebc5966b9
akane fukui <akane.1418@gmail.com>
2020-02-18 11:42:39 +0000

    rails new

---
+++ db/migrate/20200218113012_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.text :details
+      t.boolean :completed, default: false
+
+      t.timestamps
+    end
+  end
+
+  Task.create title: 'Laundry', details: 'Do not mix colors!'
+end
